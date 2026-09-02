6235fd748f10a6f62ce799452e9bc2db85064d2d
Aiman-112674 <aimanzafar348@gmail.com>
2026-09-02 14:07:35 +0500

    Added The Dataframe attributes

--- Libraries/my_pandas_practice.py
+++ Libraries/my_pandas_practice.py
@@ -15,7 +15,6 @@ import numpy as np
 # s = pd.Series([1,3,4,np.nan,6,8])
 # print(s)
 
-
 # cities = pd.Series([3000000,5000000,6000000,4000000], index = ["Isalamabad", "Lahore","Multan", "Faisalabad"])
 # print("Popular Cities")
 # print(cities)
@@ -158,84 +157,140 @@ import numpy as np
 
 # practice
 # series
-s = pd.Series([10,20,30,40] , index = ["l" , "bc" , "s" , "wq"])
-print(s)
+# s = pd.Series([10,20,30,40] , index = ["l" , "bc" , "s" , "wq"])
+# print(s)
 
-# from a list of numbers 
-s = pd.Series([10,20,30,40,50])
-print(s)
+# # from a list of numbers 
+# s = pd.Series([10,20,30,40,50])
+# print(s)
 
-#from a dictionary
-series=pd.Series(
-    {
-        "a": 2,
-        "d": 5,
-        "c": 56,
-        "f": 45,
-    }
-)
-print(series)
-#from one repeated value 
-serie = pd.Series(5, index=["a","b","c","d","f"])
-print(serie)
-# series acts like a numpy array 
-s = pd.Series([10,20,30,40,50])
-print(s)
-print(s.iloc[0])
-print(s[s>20])
-print(s*2)
+# #from a dictionary
+# series=pd.Series(
+#     {
+#         "a": 2,
+#         "d": 5,
+#         "c": 56,
+#         "f": 45,
+#     }
+# )
+# print(series)
+# #from one repeated value 
+# serie = pd.Series(5, index=["a","b","c","d","f"])
+# print(serie)
+# # series acts like a numpy array 
+# s = pd.Series([10,20,30,40,50])
+# print(s)
+# print(s.iloc[0])
+# print(s[s>20])
+# print(s*2)
 
-#series acts like a dictionary 
-#from a dictionary
-series=pd.Series(
-    {
-        "a": 2,
-        "d": 5,
-        "c": 56,
-        "f": 45,
-    }
-)
-print(series)
-print(series["a"])
-print("a" in series)
-print(series.get("f",0))
-print(series.get("g",0))
+# #series acts like a dictionary 
+# #from a dictionary
+# series=pd.Series(
+#     {
+#         "a": 2,
+#         "d": 5,
+#         "c": 56,
+#         "f": 45,
+#     }
+# )
+# print(series)
+# print(series["a"])
+# print("a" in series)
+# print(series.get("f",0))
+# print(series.get("g",0))
 
-# Math between two  series auto matches by label 
-# This is called alignment. Pandas lines up values by their label name, not their position.
-s1 = pd.Series([1,2,3], index = ["a","b","c"])
-s2 = pd.Series([10,20,30],index= ["b","c","d"] )
-# print(s1+s2)
-print(s1.add(s2, fill_value=0))
-# A series can have a name 
-s = pd.Series([1,2,3] , name = "marks")
-print(s.name)
-print(s)
+# # Math between two  series auto matches by label 
+# # This is called alignment. Pandas lines up values by their label name, not their position.
+# s1 = pd.Series([1,2,3], index = ["a","b","c"])
+# s2 = pd.Series([10,20,30],index= ["b","c","d"] )
+# # print(s1+s2)
+# print(s1.add(s2, fill_value=0))
+# # A series can have a name 
+# s = pd.Series([1,2,3] , name = "marks")
+# print(s.name)
+# print(s)
 # dataframe practice
+
+#from a dict
+import pandas as pd
+import numpy as np
 data = {
-    "name": ["Aiman","Sara"],
-    "Age": [25,30],
+    "name": ["Ali", "Sara" , "Zain"],
+    "marks": [80,90,70],
+}
+df = pd.DataFrame(data, index = [1,2,3])
+print(df)
+# Each key becomes a column, each list becomes that column's values.
+#from a list of lists
+
+data = [["Ali",80],["Sara",70],["Zain",69]]
+df = pd.DataFrame(data , columns=["name" , "marks"])
+print(df)
+
+#from a list of tuples 
+data = [("Ali",80), ("Sara",78), ("Zain",87)]
+df = pd.DataFrame(data,columns=["name","marks"],index = ["a","b","c"])
+print(df)
+
+#from a list of dictionaries 
+data = [{"name": "Ali" , "marks": 80},{"name":"Sara","marks":78},{"name":"zuhbai", "marks":67}]
+df = pd.DataFrame(data,index=["w","y","Z"])
+print(df)
+# Each dictionary = one row.
+# from a series 
+s = pd.Series([80,70,90], index = ["ALi","Zain","Sara"], name = "marks")
+df = pd.DataFrame(s)
+print(df)
+# A single Series becomes a DataFrame with one column.
+# From a dict of series 
+s1 = pd.Series([80,90,40], index = ["ALi","Zain","Sara"] )
+s2 = pd.Series([1,2,3], index=["ALi", "Zain","Sara"])
+df = pd.DataFrame({"marks":s1 , "rank":s2})
+print(df)
+
+# Selecting and Reading Data 
+data = {
+    "name": ["ALi","Zain","Sara","Nida"],
+    "marks": [90,80,70,60],
+    "city": ["Lahore","Faisalabad","Karachi","Multan"]
 }
 
-df = pd.DataFrame(data)
+df = pd.DataFrame(data, index=[1,2,3,4])
 print(df)
-# df from a list of dictoinaries 
-df_1 = pd.DataFrame([
-    {
-        "a":1,
-        "n":2,
-    },
-    {
-        "a":4,
-        "n":6,
-    }
+#selecting one column 
+print(df["name"])
+#select multiple Columns 
+print(df[["name", "marks"]])
+#select a slice of rows
+print(df[1:3])
+#filter row wirh a condition 
+print(df[df["marks"]>70])
+#filter with multiple rows
+print(df[(df["marks"]>60) & (df["city"]=="Lahore")])
+print(df[(df["marks"]>60) | (df["city"]=="Islamabad")])
 
-])
-
-print(df_1)
-#from a dict of series 
-df_2 = pd.DataFrame({
-    "one": pd.Series([1,2,3] , index = ["x", "y","z"]),
-    "two": pd.Series([4,5,6], index = ["x", "y","z"])
-})
-print(df_2)
+# Adding  , modifying ,and deleting columns 
+df["grade"] = "Pending"
+print(df)
+# Every row gets "Pending" in the new column.
+# Add a new column (calculated from another column)
+df["marks_plus_5"] = df["marks"] + 5
+print(df)
+# Add a column with a condition (Pass/Fail based on marks)
+df["result"] = df["marks"]>=60
+print(df)
+#modify an existing column 
+df["marks"] = df["marks"] +3
+print(df)
+# Delete a column --method 1 del
+del df["marks_plus_5"]
+print(df)
+# delete a column - method 2 pop
+removed_col = df.pop("result")
+print(df)
+print(removed_col)
+# Add a column without changing the original  .assign()
+df2= df.assign(marks_doubled = df["marks"] * 2)
+print(df2)
+print(df)
